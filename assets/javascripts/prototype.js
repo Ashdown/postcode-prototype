9540f408a9baf078f525162d79022f4fa55c8506
Rory Devane <rory.devane@gmail.com>
2017-09-17 23:21:56 +0100

    RD: added ajax request to get ward by postcode

--- assets/javascripts/prototype.js
+++ assets/javascripts/prototype.js
@@ -2,11 +2,39 @@
 
 (function($) {
 
-    $.ajax({
-        url: "/assets/postcodes-by-ward.json"
-    }).done(function(body) {
-        console.log('done')
-        console.log('body', body)
-    })
+    function getPostcodeData(callback) {
+        $.ajax({
+            url: "/assets/postcodes-by-ward.json"
+        }).done(function(body) {
+            callback(body);
+        })
+    }
+
+    function standardisePostcode(postcode) {
+        return postcode.replace(" ", "").toUpperCase();
+    }
+
+
+    $('.postcode-form').on('submit', function(event){
+
+        event.preventDefault();
+
+        var myPostcode = standardisePostcode($('.postcode-form .postcode-input').val());
+
+        getPostcodeData(function(postcodes){
+            var ward = false;
+
+            $.each(postcodes, function(key, postcodesForWard) {
+                $.each(postcodesForWard, function(index, postcode) {
+                    if(postcode === myPostcode) {
+                        ward = key;
+                    }
+                });
+            });
+
+            console.log('ward', ward);
+        });
+
+    });
 
 })(jQuery)
